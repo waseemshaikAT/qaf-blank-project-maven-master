8e4f939fdb88c17c3a2ca453fdf78f81e1ed8bc8
waseemshaikAT <shaikwasim4u@gmail.com>
2023-01-07 09:27:49 +0530

    second push

--- src/test/java/com/qmetry/qaf/example/test/SampleTest.java
+++ src/test/java/com/qmetry/qaf/example/test/SampleTest.java
@@ -2,6 +2,7 @@ package com.qmetry.qaf.example.test;
 
 import static com.qmetry.qaf.automation.step.CommonStep.get;
 import static com.qmetry.qaf.automation.step.CommonStep.verifyLinkWithPartialTextPresent;
+import static com.qmetry.qaf.automation.step.client.RuntimeScenarioFactory.scenario;
 import static com.qmetry.qaf.example.steps.StepsLibrary.searchFor;
 import org.testng.annotations.Test;
 
@@ -14,5 +15,15 @@ public class SampleTest extends WebDriverTestCase {
 		get("/");
 		searchFor("qaf github infostretch");
 		verifyLinkWithPartialTextPresent("qaf");
+	}
+	@Test
+	public void reRun(){
+		scenario().given("I am lauching chrome page",()-> {
+			get("/");
+		}).when("I am searching for git hub",()->{
+			searchFor("qaf github infostretch");
+		}).then("I verify link",()->{
+			verifyLinkWithPartialTextPresent("qaf");
+		}).execute();
 	}
 }
